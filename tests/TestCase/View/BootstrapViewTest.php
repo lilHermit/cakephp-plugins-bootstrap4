c6b4bf4545cafb8bd6ea095c1249b55f8c80ea09
lilHermit <developer@lilhermit.co.uk>
2017-02-09 15:16:22 +0000

    Renamed ViewTest to BootstrapViewTest Made sure BoostrapView was used in all tests Updated .gitignore

---
+++ tests/TestCase/View/BootstrapViewTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace lilHermit\Bootstrap4\Test\TestCase\View;
+
+class ViewTest extends \Cake\Test\TestCase\View\ViewTest {
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->View = $this->PostsController->createView('TestApp\View\AppView');
+        $this->View->viewPath = 'Posts';
+
+        $this->ThemeView = $this->ThemePostsController->createView('TestApp\View\AppView');
+        $this->ThemeView->viewPath = 'Posts';
+    }
+
+}
--- tests/TestCase/View/ViewTest.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace lilHermit\Bootstrap4\Test\TestCase\View;
-
-class ViewTest extends \Cake\Test\TestCase\View\ViewTest {
-
-}
