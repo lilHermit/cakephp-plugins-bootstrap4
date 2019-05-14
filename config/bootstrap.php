fee9da1b82a018fe8957846a2531dcc389457f4c
Ali Bakir <ali.m.bakir@gmail.com>
2019-05-14 10:16:55 +0100

    Use Immutable objects as now recommended

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -4,5 +4,9 @@ use Cake\Database\Type;
 
 // Allow disabling of html5-datetime-type
 if (Configure::read('lilHermit-plugin-bootstrap4.disable-html5-datetime-type') !== true) {
+    $immutable = Type::build('datetime')->getDateTimeClassName() === 'Cake\I18n\FrozenTime';
     Type::map('datetime', 'LilHermit\Bootstrap4\Database\Type\Html5DateTimeType');
+    if ($immutable) {
+        Type::build('datetime')->useImmutable();
+    }
 }
